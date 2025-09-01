ea559241faadbd35d7ecb798318ee46708bb10eb
Bombo <bombozaur6@gmail.com>
2025-09-01 14:24:30 +0200

    updates Translate updates

--- web/src/app/plants-collection-component/plant-detail-component/updates-component/update/update-component.ts
+++ web/src/app/plants-collection-component/plant-detail-component/updates-component/update/update-component.ts
@@ -1,11 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
 import {PlantUpdate} from '../../../../Interfaces/Plants/plant-update';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'tr[app-update-component]',
   imports: [
-    TranslatePipe
+    TranslatePipe,
+    DatePipe
   ],
   templateUrl: './update-component.html',
   styleUrls: ['./update-component.css', '../../Card.css']
@@ -13,4 +15,7 @@ import {PlantUpdate} from '../../../../Interfaces/Plants/plant-update';
 export class UpdateComponent {
   @Input() update!: PlantUpdate;
 
+  protected dateFields: string[] = ['purchaseDate', 'fertilizationDate', 'wateringDate', 'creationDate'];
+  protected choiceFields: string[] = ['plantCondition', 'plantSoil', 'plantWatering', 'plantBerth', 'plantToxicity', 'plantLifeExpectancy'];
+  protected boolFields: string[] = ['alive', 'shared'];
 }
